94c276858f7acbde8f70588f47e91914084d2525
slkennedy <slkennedy@gmail.com>
2014-09-17 00:05:37 -0400

    Inital commit

---
+++ app/scripts/main.js
@@ -0,0 +1,32 @@
+function reusableFunction(container, templateID, model) {
+    var templateFunction = _.template($('#' + templateID).text());
+    var renderTemplate = templateFunction(model);
+    $(container).append(renderTemplate);
+}
+
+$.ajax("https://api.github.com/issues", {
+    type: 'GET',
+    datatype: 'json'
+}).done(function(issues) {
+    _.each(issues, function(issue) {
+        reusableFunction('.list-items', 'template-list', issue);
+    });
+
+});
+
+setInterval (function(){
+	$(document).on('click', '.titlelink', function() {
+		var title = $(this).text();
+	    $.ajax({
+	        type: 'GET',
+	        datatype: 'json',
+	        url: $(this).attr('data-id')
+	    }).done(function(issues) {
+	    	console.log(issues)
+		       _.each(issues, function(issue) {
+	        	issue.commentTitle = title;
+	            reusableFunction('.list-details', 'template-details', issue)
+        	});
+    	});
+	});
+}, 10000);
